92e7854a0c52df3aa9cfc16c97d759f2b3cf2bd4
VikasGaud97 <117566733+VikasGaud97@users.noreply.github.com>
2023-07-30 20:00:56 +0530

    Update data.js

--- database/data.js
+++ database/data.js
@@ -3,14 +3,13 @@ import dotenv from "dotenv";
 
 
 dotenv.config();
-const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD;
+const DATABASE = process.env.DB_DATABASE;
+// const PASSWORD = process.env.DB_PASSWORD;
 
 
 const DataConnection = () => {
-    const MONGODB_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.obhdw1t.mongodb.net/?retryWrites=true&w=majority`;
 
-    mongoose.connect(MONGODB_URL, { useNewUrlParser: true })
+    mongoose.connect(DATABASE, { useNewUrlParser: true })
     .then(()=>{
       console.log("data connection is sucessful with mongoDB database")
     }).catch((err)=>{
